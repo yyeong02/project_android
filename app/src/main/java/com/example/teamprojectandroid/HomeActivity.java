1569cfcfbe5cdfeb42bd172c12e63aa6192e715c
Geunyeong Yu <i517099@fhict.nl>
2023-11-06 22:13:48 +0900

    project upload

---
+++ app/src/main/java/com/example/teamprojectandroid/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.teamprojectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/teamprojectandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.teamprojectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        moveToHome(3000);
+    }
+
+    private void moveToHome(int time){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },time);
+    }
+}
