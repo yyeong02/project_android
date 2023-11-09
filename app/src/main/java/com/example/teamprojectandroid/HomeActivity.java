ce94351041d28a39d78f10d3f020196260752d68
MinUkKoo <koomu09@naver.com>
2023-11-09 12:14:19 +0900

    Bottom Menu

--- app/src/main/java/com/example/teamprojectandroid/HomeActivity.java
+++ app/src/main/java/com/example/teamprojectandroid/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.teamprojectandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +8,49 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.google.android.material.navigation.NavigationBarView;
+
 public class HomeActivity extends AppCompatActivity {
+
+    CalFragment calFragment;
+    Medicine_boxFragment medicine_boxFragment;
+    PharFragment pharFragment;
+    QNAFragment qnaFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        calFragment = new CalFragment();
+        medicine_boxFragment = new Medicine_boxFragment();
+        pharFragment = new PharFragment();
+        qnaFragment = new QNAFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, calFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.cal:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, calFragment).commit();
+                        return true;
+                    case R.id.medicine_box:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, medicine_boxFragment).commit();
+                        return true;
+                    case R.id.phar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, pharFragment).commit();
+                        return true;
+                    case R.id.qna:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, qnaFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
 
