bf57c0b4986b438fd89a95f95a8d98bbf1ac1a9f
MinUkKoo <koomu09@naver.com>
2023-11-09 12:13:22 +0900

    Bottom Menu

---
+++ app/src/main/java/com/example/teamprojectandroid/CalFragment.java
@@ -0,0 +1,21 @@
+package com.example.teamprojectandroid;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CalFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cal, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/teamprojectandroid/QNAFragment.java
@@ -0,0 +1,18 @@
+package com.example.teamprojectandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class QNAFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_q_n_a, container, false);
+    }
+}
