5b8779c52ed2d556013df9aac886fbf140080d9d
MinUkKoo <koomu09@naver.com>
2023-11-10 21:25:31 +0900

    MapAPI

--- app/src/main/java/com/example/teamprojectandroid/MapActivity.java
+++ app/src/main/java/com/example/teamprojectandroid/MapActivity.java
@@ -1,16 +1,9 @@
 package com.example.teamprojectandroid;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -19,22 +12,10 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        /* 키 해시 얻기*/
+        MapView mapView = new MapView(this);
 
-        try{
-            PackageInfo info = getPackageManager().getPackageInfo("com.example.teamprojectandroid", PackageManager.GET_SIGNING_CERTIFICATES);
-
-            Signature[] signatures = info.signingInfo.getApkContentsSigners();
-
-            for (Signature signature : signatures){
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
+
