8d73dc22f7b586059f1f781ad0659a2327c71033
MinUkKoo <koomu09@naver.com>
2023-11-12 16:54:08 +0900

    MapAPI

--- app/src/main/java/com/example/teamprojectandroid/Medicine_boxFragment.java
+++ app/src/main/java/com/example/teamprojectandroid/Medicine_boxFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.util.Log;
 import android.widget.RelativeLayout;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import androidx.fragment.app.Fragment;
@@ -21,15 +23,27 @@ public class Medicine_boxFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_medicine_box, container, false);
 
-        MapView mapView = new MapView(requireContext());
-
-        mapViewContainer = v.findViewById(R.id.map_view);
-        if(mapViewContainer != null){
-            mapViewContainer.addView(mapView);
-        } else{
-            Log.e("Medicine_boxFragment", "mapViewContainer is null"); //로그 에러 메세지
-        }
+        //지도 띄우기
+        MapView mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup) v.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        // 중심점 변경 - 예제 좌표는 영남대학교 경산캠퍼스
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.8318190753964, 128.75853345073014), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(4,true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithCONGCoord(35.8318190753964, 128.75853345073014);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); //기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); //마커를 클릭했을 때, 기본적으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
         return v;
     }
 }
