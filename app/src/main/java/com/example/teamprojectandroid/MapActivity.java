f82d95fae7d5e46a30f0f0b8adf1028f47c907f1
MinUkKoo <koomu09@naver.com>
2023-11-12 15:17:20 +0900

    MapAPI

--- app/src/main/java/com/example/teamprojectandroid/MapActivity.java
+++ app/src/main/java/com/example/teamprojectandroid/MapActivity.java
@@ -2,10 +2,14 @@ package com.example.teamprojectandroid;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
 import net.daum.mf.map.api.MapView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity{
+
+    public RelativeLayout mapViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +22,3 @@ public class MapActivity extends AppCompatActivity {
         mapViewContainer.addView(mapView);
     }
 }
-
