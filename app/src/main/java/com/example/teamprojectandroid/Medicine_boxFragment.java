8785b5dfa8d6b5577c10e95061dfccdd9ae61ec2
MinUkKoo <koomu09@naver.com>
2023-11-12 16:29:37 +0900

    MapAPI

--- app/src/main/java/com/example/teamprojectandroid/Medicine_boxFragment.java
+++ app/src/main/java/com/example/teamprojectandroid/Medicine_boxFragment.java
@@ -1,17 +1,35 @@
 package com.example.teamprojectandroid;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
+
+import androidx.fragment.app.Fragment;
 
 
 public class Medicine_boxFragment extends Fragment {
 
+    RelativeLayout mapViewContainer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicine_box, container, false);
+        View v = inflater.inflate(R.layout.fragment_medicine_box, container, false);
+
+        MapView mapView = new MapView(requireContext());
+
+        mapViewContainer = v.findViewById(R.id.map_view);
+        if(mapViewContainer != null){
+            mapViewContainer.addView(mapView);
+        } else{
+            Log.e("Medicine_boxFragment", "mapViewContainer is null"); //로그 에러 메세지
+        }
+        mapViewContainer.addView(mapView);
+        return v;
     }
 }
--- app/src/main/java/com/example/teamprojectandroid/PharFragment.java
+++ app/src/main/java/com/example/teamprojectandroid/PharFragment.java
@@ -1,16 +1,35 @@
 package com.example.teamprojectandroid;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
+
+import androidx.fragment.app.Fragment;
+
 
 public class PharFragment extends Fragment {
+
+    RelativeLayout mapViewContainer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phar, container, false);
+        View v =  inflater.inflate(R.layout.fragment_phar, container, false);
+
+        MapView mapView = new MapView(requireContext());
+
+        mapViewContainer = v.findViewById(R.id.map_view);
+        if (mapViewContainer != null){
+            mapViewContainer.addView(mapView);
+        } else{
+            Log.e("PharFragment", "mapViewContainer is null"); //로그 에러 메시지 확인
+        }
+        mapViewContainer.addView(mapView);
+        return v;
     }
-}
+    }
